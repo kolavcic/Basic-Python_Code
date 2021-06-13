114d74240dbfb354d10aa54503d9b705f797b7b4
Mladen <mladen@tyk.io>
2021-06-13 18:49:59 +0200

    cas broj 4

--- Code Academy/4. cas/zadatak9.py
+++ Code Academy/4. cas/zadatak9.py
@@ -1,3 +1,11 @@
 # Napisati program koji za uneti pozitivan ceo broj n, ispisuje zvezdice i tako iscrtava odgovarajuću sliku. 
 # Slika predstavlja pravougli trougao sastavljen od zvezdica. 
 # Kateta trougla je dužine n, a prav ugao nalazi se u gornjem levom uglu slike.
+
+n = int(input("Unesite broj: "))
+
+for i in reversed(range(0, n)):
+    for j in reversed(range(0, i+1)):
+            print("*",end="")
+    print()
+
